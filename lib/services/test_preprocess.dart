666db761977e068083c66dc2defffe0fdc8df6ed
itsviv0 <vivekganeshs12@gmail.com>
2025-05-15 22:29:23 +0530

    Fixed the test case for fetch and detect.

--- lib/services/test_preprocess.dart
+++ lib/services/test_preprocess.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 void main() async {
   final extractedText =
-      "INGREDIENTS: Toned Milk (89), Sugar, Flavor (Nature Identical and Artificial (Kaju Katri) Flavoring Substances), Stabilizers (INS 415, INS 412, INS 407), Sequestrant (INS 339 (ii)), Emulsifier (INS 471), Cashew Powder (0.05%), lodized Salt, and Synthetic Food Color (INS 110). Allergen Advice: Contains Milk & Nut";
+      "INGREDIENTS: tonedMilk (89), Sugar, Flavor (Nature Identical and Artificial (Kaju Katri) Flavoring Substances), Stabilizers (INS 415, INS 412, INS 407), Sequestrant (INS 339 (ii)), Emulsifier (INS 471), Cashew Powder (0.05%), lodized Salt, and Synthetic Food Color (INS 110). Allergen Advice: Contains Milk & Nut";
   final baseUrl = Uri.parse('https://preprocesstextsafeeats.vercel.app/');
 
   final encodedText = Uri.encodeComponent(extractedText);
@@ -13,29 +13,7 @@ void main() async {
   // if (response.statusCode == 200) {
   final data = json.decode(response.body);
   final listIngredients = data['ingredients'];
-  final List<String> result = [];
-
-  for (final ingredient in listIngredients) {
-    result.add(ingredient.toString());
-  }
-
-  print(result);
-
-  // allergy
-
-  const String baseUrlAllergy = 'https://allergydetectionsafeeats.vercel.app/';
-
-  final urlAllergy = Uri.parse('$baseUrlAllergy/allergy');
-
-  final responseAllergy = await http.post(
-    urlAllergy,
-    headers: {'Content-Type': 'application/json'},
-    body: jsonEncode({'ingredients': result}),
-  );
-  // if (responseAllergy.statusCode == 200) {
-  final resultAllergy = jsonDecode(responseAllergy.body);
-  print('Allergy Results:');
-  resultAllergy.forEach((ingredient, allergy) {
-    print('$ingredient → $allergy');
-  });
+  print(listIngredients);
+  final listAllergens = data['allegens'];
+  print(listAllergens);
 }
