315194c64d5f01400f0215298f41d4382cb651bd
itsviv0 <vivekganeshs12@gmail.com>
2025-07-30 22:56:41 +0530

    Updated base URL for ingredient fetching to the correct endpoint.

--- lib/services/test_preprocess.dart
+++ lib/services/test_preprocess.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 void main() async {
   final extractedText =
       "INGREDIENTS: toned Milk (89), Sugar, Flavor (Nature Identical and Artificial (Kaju Katri) Flavoring Substances), Stabilizers (INS 415, INS 412, INS 407), Sequestrant (INS 339 (ii)), Emulsifier (INS 471), Cashew Powder (0.05%), lodized Salt, and Synthetic Food Color (INS 110). Allergen Advice: Contains Milk & Nut";
-  final baseUrl = Uri.parse('https://preprocesstextsafeeats.vercel.app/');
+  final baseUrl = Uri.parse('https://allergydetectionsafeeats.vercel.app/');
 
   final encodedText = Uri.encodeComponent(extractedText);
   final url = '$baseUrl/preprocess?ocr_text=$encodedText';
